2200816fb16243cdf63c02c1077f54e32f8aeef2
Gabriele Pozzati <x_gabpo@tetralith1.nsc.liu.se>
2022-05-24 14:01:59 +0200

    runner

--- src/AFrunner_default.sh
+++ src/AFrunner_default.sh
@@ -8,14 +8,10 @@
 #SBATCH --gpus-per-task=1
 #SBATCH -t 12:00:00
 
-export SINGULARITYENV_NVIDIA_VISIBLE_DEVICES="all"
-export SINGULARITYENV_TF_FORCE_UNIFIED_MEMORY="1"
-export SINGULARITYENV_XLA_PYTHON_CLIENT_MEM_FRACTION="4.0"
-
 ##### AF2 CONFIGURATION #####
-COMMON="/proj/snic2019-35-62/users/x_gabpo/"
+COMMON="/proj/snic2019-35-62/"
 AFHOME="$COMMON/af2-v2.2.0"           		        # Path of AF2-multimer-mod directory.
-DATAHOME="$COMMON"					# Path of data folder
+DATAHOME="$COMMON/users/x_gabpo"			# Path of data folder
 SINGULARITY="$AFHOME/AF_data_v220/alphafold_v220.sif"   # Path of singularity image.
 PARAM="$AFHOME/AF_data_v220/"                           # path of param folder containing AF2 Neural Net parameters.
 MAX_RECYCLES=3
@@ -32,22 +28,19 @@ POS=$(($SLURM_ARRAY_TASK_ID + $OFFSET))
 PDB=`tail -n+$POS $LIST | head -n 1`
 echo ${DATA}${PDB}
 
-FILELIST=`ls ${DATA}${PDB} | grep "fasta"`
-for FILE in $FILELIST; do
-    cp $DATA/${PDB}/$FILE $DATA/${PDB}/${PDB}.fasta
-    singularity exec --nv --bind $COMMON:$COMMON $SINGULARITY \
-        python3 $AFHOME/alphafold/run_alphafold.py \
-            --fasta_paths=$DATA/${PDB}/${PDB}.fasta \
-            --model_preset=$MODEL_SET \
-            --output_dir=$DATA \
-            --data_dir=$PARAM \
-	    --recycles=$MAX_RECYCLES \
-            --db_preset=reduced_dbs \
-            --num_multimer_predictions_per_model=1
+singularity exec --nv --bind $COMMON:$COMMON \
+    --env="NVIDIA_VISIBLE_DEVICES=all" \
+    --env="TF_FORCE_UNIFIED_MEMORY=1" \
+    --env="XLA_PYTHON_CLIENT_MEM_FRACTION=4.0" \
+    --env="OPENMM_CPU_THREADS=32" \
+    --env="MAX_CPUS=32" \
+    $SINGULARITY \
+    python3 $AFHOME/alphafold/run_alphafold.py \
+        --fasta_paths=$DATA/${PDB}.fasta \
+        --model_preset=$MODEL_SET \
+        --output_dir=$DATA \
+        --data_dir=$PARAM \
+	--recycles=$MAX_RECYCLES \
+        --db_preset=reduced_dbs \
+        --num_multimer_predictions_per_model=1
 
-    mkdir $DATA/${PDB}/${FILE:0:6}
-    mv $DATA/${PDB}/${PDB}.fasta $DATA/${PDB}/${FILE:0:6}/
-    mv *.pdb $DATA/${PDB}/${FILE:0:6}/
-    mv *.pkl $DATA/${PDB}/${FILE:0:6}/
-
-done;
